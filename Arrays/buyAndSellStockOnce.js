afba6533015e4cb18024549f363e47c065e42f81
JBayona <jordavids_22@hotmail.com>
2019-02-15 22:29:49 -0800

    Buy and Sell Stock Once.

---
+++ Arrays/buyAndSellStockOnce.js
@@ -0,0 +1,26 @@
+/*
+Tenemos un arreglo en donde representa el valor de stocks al día, debemos calcular cuánto es la mayor ganancia
+que podemos obtener considerando que sólo podemos comprar una vez y vender una vez.
+
+Arreglo temporal de las ganancias por día
+*/
+
+function buyAndSellStockOnce(stocks) {
+let minStockPrice = Number.MAX_SAFE_INTEGER;
+  let todayStockProfit = 0;
+  let maxStockPrice = 0;
+  let tmp = [];
+  
+  for(let i = 0; i < stocks.length; i++) {
+   todayStockProfit = stock[i] - minStockPrice;
+    maxStockPrice = Math.max(maxStockPrice, todayStockProfit);
+    minStockPrice = Math.min(stock[i], minStockPrice);
+    tmp.push(maxStockPrice);
+  }
+  
+  console.log(tmp);
+  return maxStockPrice;
+}
+
+stock = [310, 315, 275, 295, 260, 270, 290, 230, 255, 250];
+console.log(buyAndSellStockOnce(stock));
